71e5a8098b0d455971f7b0664c5130955def92f2
Thales Souza <thalessouza1904@gmail.com>
2024-08-30 19:28:47 -0300

    wtf bro

--- copycat.py
+++ copycat.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from sqlalchemy import create_engine
 
 class DatabaseConnector:
@@ -12,18 +13,29 @@ class DatabaseConnector:
     def connect(self):
         try:
             if self.db_type == 'mysql':
-                url = f'mysql+mysqlconnector://{self.user}:{self.password}@{self.host}/{self.database}'
+                import mysql.connector
+                self.engine = mysql.connector.connect(
+                    host=self.host,
+                    user=self.user,
+                    password=self.password,
+                    database=self.database
+                )
             elif self.db_type == 'fdb':
-                url = f'firebird+fdb://{self.user}:{self.password}@{self.host}/{self.database}'
+                import fdb
+                self.engine = fdb.connect(
+                    host=self.host,
+                    user=self.user,
+                    password=self.password,
+                    database=self.database
+                )
             else:
                 raise TypeError("Database not found. Try again with the specified types in github.com/thalessz/db_copycat.git")
-            self.engine = create_engine(url)
         except Exception as e:
             raise Exception(f"ERROR: {e}")
 
     def close_connection(self):
         if self.engine:
-            self.engine.dispose()
+            self.engine.close()
             print("Connection Closed")
 
 class MigrationManager:
@@ -31,44 +43,92 @@ class MigrationManager:
         self.source_connector = DatabaseConnector(source_config)
         self.target_connector = DatabaseConnector(target_config)
 
-    def clone_database(self, table_name, columns=None):
-        with self.source_connector.engine.connect() as source_conn, self.target_connector.engine.connect() as target_conn:
+    def create_table(self, table_name, columns=None):
+        self.source_connector.connect()
+        self.target_connector.connect()
+
+        try:
+            # Obter a estrutura da tabela
+            table_structure = self.get_table_structure(table_name)
+            self.create_table_in_target(table_name, table_structure, self.target_connector.engine)
+        finally:
+            self.source_connector.close_connection()
+            self.target_connector.close_connection()
+
+    def insert_data(self, table_name, columns=None):
+        self.source_connector.connect()
+        self.target_connector.connect()
+
+        try:
+            # Se colunas foram especificadas, use-as na consulta
             if columns:
                 source_query = f"SELECT {', '.join(columns)} FROM {table_name}"
             else:
                 source_query = f'SELECT * FROM {table_name}'
 
-            table_structure = self.get_table_structure(table_name)
-            self.create_table(table_name, table_structure, target_conn)
+            # Carregar dados em um DataFrame
+            df = pd.read_sql(source_query, self.source_connector.engine)
 
-            result = source_conn.execute(source_query)
-            for row in result:
-                insert_query = f'INSERT INTO {table_name} VALUES ({", ".join(["%s"] * len(row))})'
-                target_conn.execute(insert_query, row)
+            # Tratar dados de data/hora
+            if 'LOCAL' in df.columns:
+                df['LOCAL'] = pd.to_datetime(df['LOCAL'], errors='coerce')  # Converte e trata erros
+                df = df.dropna(subset=['LOCAL'])  # Remove linhas com valores inválidos
 
-    def get_table_structure(self, table_name):
-        with self.source_connector.engine.connect() as conn:
-            structure = f"SELECT RDB$FIELD_NAME, RDB$TYPE, RDB$NULL_FLAG FROM RDB$RELATION_FIELDS WHERE RDB$RELATION_NAME = '{table_name}'"
-            stResult = conn.execute(structure)
-            return stResult.fetchall()
+            # Tratar valores ausentes
+            df.fillna({'COLUMN_NAME': 'default_value'}, inplace=True)  # Substitua 'COLUMN_NAME' e 'default_value' conforme necessário
 
-    @staticmethod
-    def create_table(table_name, structure, conn):
+            # Remover duplicatas
+            df.drop_duplicates(inplace=True)
+
+            # Inserir os dados tratados no banco de dados de destino
+            cursor = self.target_connector.engine.cursor()
+            for index, row in df.iterrows():
+                insert_query = f'INSERT INTO {table_name} ({", ".join(df.columns)}) VALUES ({", ".join(["%s"] * len(row))})'
+                cursor.execute(insert_query, tuple(row))
+
+            cursor.close()
+        finally:
+            self.source_connector.close_connection()
+            self.target_connector.close_connection()
+
+    def create_table_in_target(self, table_name, structure, conn):
+        cursor = conn.cursor()
         columns = []
         for field_name, field_type, null_flag in structure:
             sql_type = {
-                7: 'INTEGER',
-                8: 'FLOAT',
-                9: 'DECIMAL',
-                10: 'VARCHAR',
-                11: 'CHAR',
-                12: 'DATE',
-                13: 'TIME',
-                14: 'TIMESTAMP'
-            }.get(field_type, 'UNKNOWN')
+                7: 'INT',          # INTEGER
+                8: 'FLOAT',        # FLOAT
+                9: 'DECIMAL',      # DECIMAL
+                10: 'VARCHAR(255)', # VARCHAR
+                11: 'CHAR(255)',   # CHAR
+                12: 'DATE',        # DATE
+                13: 'TIME',        # TIME
+                14: 'DATETIME'     # TIMESTAMP
+            }.get(field_type, 'TEXT')  # Usar TEXT como padrão se o tipo for desconhecido
 
             null_constraint = 'NOT NULL' if null_flag == 0 else ''
-            columns.append(f'{field_name} {sql_type} {null_constraint}')
+            columns.append(f'{field_name} {sql_type} {null_constraint}'.strip())
 
         ct_command = f'CREATE TABLE {table_name} ({", ".join(columns)})'
-        conn.execute(ct_command)
+        print(f"Executing SQL: {ct_command}")  # Para depuração
+        cursor.execute(ct_command)  # Use o cursor para executar a consulta
+        cursor.close()  # Feche o cursor após a execução
+
+    def get_table_structure(self, table_name):
+        conn = self.source_connector.engine.cursor()
+        try:
+            structure = f"""
+            SELECT 
+                RDB$RELATION_FIELDS.RDB$FIELD_NAME AS field_name,
+                RDB$FIELDS.RDB$FIELD_TYPE AS field_type,
+                RDB$RELATION_FIELDS.RDB$NULL_FLAG AS null_flag
+            FROM 
+                RDB$RELATION_FIELDS
+                    JOIN RDB$FIELDS ON RDB$RELATION_FIELDS.RDB$FIELD_SOURCE = RDB$FIELDS.RDB$FIELD_NAME  
+                    WHERE 
+                        RDB$RELATION_FIELDS.RDB$RELATION_NAME = ?
+            """
+            conn.execute(structure, (table_name,))
+            return conn.fetchall()
+        finally:
+            conn.close()
--- main.py
+++ main.py
@@ -1,17 +1,19 @@
-from sqlalchemy.testing.plugin.plugin_base import logging
-
-from copycat import *
-import os
+import logging
 from dotenv import load_dotenv
+from copycat import MigrationManager
+import os
 
 load_dotenv()
+
 def main():
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s: %(message)s')
+
     source_config = {
-    'DB_HOST': os.getenv('FDB_HOST'),
-    'DB_USER': os.getenv('FDB_USER'),
-    'DB_PASSWORD': os.getenv('FDB_PASSWORD'),
-    'DB_DATABASE': os.getenv('FDB_DATABASE'),
-    'DB_TYPE': 'fdb'
+        'DB_HOST': os.getenv('FDB_HOST'),
+        'DB_USER': os.getenv('FDB_USER'),
+        'DB_PASSWORD': os.getenv('FDB_PASSWORD'),
+        'DB_DATABASE': os.getenv('FDB_DATABASE'),
+        'DB_TYPE': 'fdb'
     }
 
     target_config = {
@@ -20,21 +22,30 @@ def main():
         'DB_PASSWORD': os.getenv('SQL_PASSWORD'),
         'DB_DATABASE': os.getenv('SQL_DATABASE'),
         'DB_TYPE': 'mysql'
-        }
+    }
 
     migrationManager = MigrationManager(source_config, target_config)
+
     try:
         migrationManager.source_connector.connect()
         migrationManager.target_connector.connect()
+
         table_name = 'FUNCIONARIO'
-        migrationManager.clone_database(table_name)
-        print(f'Table {table_name.lower()} succesfully cloned!')
+        columns = ['CODIGO', 'NOMECOMPLETO', 'LOCAL', 'FUNCAO', 'SENHA']
+
+        logging.info(f'Criando a tabela {table_name.lower()} no banco de dados de destino...')
+        migrationManager.create_table(table_name, columns)
+        logging.info(f'Tabela {table_name.lower()} criada com sucesso!')
+
+        logging.info(f'Inserindo dados na tabela {table_name.lower()}...')
+        migrationManager.insert_data(table_name, columns)
+        logging.info(f'Dados inseridos na tabela {table_name.lower()} com sucesso!')
+
     except Exception as e:
-        print(f'ERROR: {e}')
+        logging.error(f'Erro durante a clonagem da tabela {table_name.lower()}: {e}')
     finally:
         migrationManager.source_connector.close_connection()
         migrationManager.target_connector.close_connection()
 
 if __name__ == "__main__":
     main()
-
